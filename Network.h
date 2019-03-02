986aeb785f7b31d2b0258392a4f23e17511a9458
G6EJD <d.l.bird@sky.com>
2019-03-02 12:46:50 +0000

    Update Network.h

--- Network.h
+++ Network.h
@@ -5,11 +5,11 @@ IPAddress local_IP(192, 168, 0, 175); // Set your server's fixed IP address here
 IPAddress gateway(192, 168, 0, 1);    // Set your network Gateway usually your Router base address
 IPAddress subnet(255, 255, 255, 0); 
 IPAddress dns(192,168,0,1);           // Set your network DNS usually your Router base address
-const char ssid_1[]     = "SKYAE43F";
-const char password_1[] = "QDMSWSFMQL";
+const char ssid_1[]     = "your_SSID1";
+const char password_1[] = "your_PASSWORD_for SSID1";
 
-const char ssid_2[]     = "SKYAE43F";
-const char password_2[] = "QDMSWSFMQL";
+const char ssid_2[]     = "your_SID2";
+const char password_2[] = "your_PASSWORD_for SSID2";
 
 const char ssid_3[]     = "your_SSID3";
 const char password_3[] = "your_PASSWORD_for SSID3";
